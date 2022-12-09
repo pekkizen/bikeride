2a9e21c8c672d9d3702380b9e07b5e656f70fff1
pekkizen <60615893+pekkizen@users.noreply.github.com>
2022-12-09 13:40:10 +0200

    Add files via upload

---
+++ param/param.go
@@ -0,0 +1,588 @@
+package param
+
+import (
+	"encoding/json"
+	"os"
+	// "fmt"
+)
+
+const (
+	kmh2ms  = 1.0 / 3.6
+	ms2kmh  = 3.6
+	min2sec = 60.0
+	sec2min = 1.0 / 60.0
+)
+
+type logger interface {
+	Msg(level int, v ...interface{}) error
+	Err(v ...interface{}) error
+	Errorf(format string, v ...interface{}) error
+	Errors() int
+	SetPrefix(string)
+}
+
+// Parameters on --
+type Parameters struct {
+	Filter      filter
+	Weight      weight      `json:"weight (kg)"`
+	UphillBreak uphillBreak `json:"uphillBreaks"`
+	Powermodel  powermodel
+
+	riderAndBike
+	rideControl
+	environment
+	calculation
+	filesEtc
+}
+
+type filter struct {
+	Rounds          int
+	InitialRelGrade float64 `json:"initialRelativeGrade"`
+	MinRelGrade     float64 `json:"minRelativeGrade"`
+	IpoSumDist      float64 `json:"interpolateSumDist"`
+	IpoDist         float64 `json:"interpolateDist"`
+	BackstepRounds  int
+	BackstepDist    float64
+	Backsteps       int
+	LevelFactor     float64
+	LevelMax        float64
+	LevelMin        float64
+	MinSegDist      float64 `json:"minSegmentDist"`
+	MaxFilteredEle  float64 `json:"maxFilteredElevation"`
+}
+
+// DiffCalc	= 1 stepping delta velocity
+// DiffCalc	= 2 stepping delta distance
+// DiffCalc	= 3 stepping delta time
+
+type calculation struct {
+	DeltaVel  float64
+	DeltaTime float64
+	DiffCalc  int `json:"diffCalc"`
+	VelSolver int
+
+	NRtol        float64 `json:"velTolerance"`
+	Bracket      float64 `json:"velBracket"`
+	VelGuess     float64
+	ReportTech   bool
+	CalcVelError bool
+	UseVelTable  bool
+	MinPower     float64
+	// SameVelTol        float64 `json:"sameVelTol (m/s)"`
+	SingleStepBraking bool
+	PowerIn           float64
+	PowerOut          float64
+	VerticalUpGrade   float64
+}
+
+type filesEtc struct {
+	RideJSON        string
+	ConfigJSON      string
+	GPXfile         string
+	RouteName       string
+	GPXdir          string
+	ResultDir       string
+	RouteCSV        bool
+	ResultTXT       bool
+	ResultJSON      bool
+	Logfile         string
+	ParamOutJSON    bool
+	UseCRLF         bool
+	CSVuseComma     bool
+	GPXuseXMLparser bool
+	GPXignoreErrors bool
+	// CSVdesimals     int // not used
+	Display        bool
+	LogMode        int
+	LogLevel       int
+	CheckParams    bool
+	CheckRideSetup bool
+}
+
+type rideControl struct {
+	MaxSpeed           float64 `json:"maxSpeed (km/h)"`
+	MinSpeed           float64 `json:"minSpeed (km/h)"`
+	MinLimitedSpeed    float64 `json:"minLimitedSpeed (km/h)"`
+	VelLimitGrade      float64
+	SteepDownhillGrade float64
+	MinFreewheelPower  float64 `json:"minFreewheelingPower (w)"`
+	HeavyBrakingPower  float64 `json:"heavyBrakingPower (w)"`
+	MaxFreewheelPower  float64 `json:"maxFreewheelingPower (w)"`
+	MinPedalledGrade   float64
+
+	LimitTurnSpeeds   bool
+	LimitDownSpeeds   bool
+	LimitEntrySpeeds  bool
+	VerticalDownSpeed float64 `json:"verticalDownSpeed (m/h)"`
+	BrakingDist       float64 `json:"brakingDist (m)"`
+	PowerAcceCoef     float64 `json:"accelerationPowerFactor"`
+	PowerAcceMinPros  float64 `json:"minAccelerationPower (%)"`
+	PowerDeceCoef     float64 `json:"decelerationPowerFactor"`
+	SpeedDeceCoef     float64 `json:"decelerationSpeedFactor"`
+}
+
+type weight struct {
+	Rider   float64
+	Bike    float64
+	Luggage float64
+	Total   float64
+	Wheels  float64
+}
+
+type uphillBreak struct {
+	PowerLimit    float64 `json:"powerLimit (%)"`
+	ClimbDuration float64 `json:"climbDuration (min)"`
+	BreakDuration float64 `json:"breakDuration (min)"`
+}
+
+type powermodel struct {
+	PowermodelType int `json:"powerModel"`
+
+	FlatSpeed float64 `json:"flatSpeed (km/h)"`
+	FlatPower float64 `json:"flatPower (w)"`
+
+	UphillPowerGrade float64
+	UphillPowerSpeed float64 `json:"uphillPowerSpeed (km/h)"`
+	UphillPower      float64 `json:"uphillPower (w)"`
+	VerticalUpSpeed  float64 `json:"verticalUpSpeed (m/h)"`
+
+	DownhillPowerGrade float64
+	DownhillPower      float64 `json:"downhillPower (%)"`
+	MaxPedalledSpeed   float64 `json:"maxPedalledSpeed (km/h)"`
+
+	SysTailwind           float64 `json:"sysTailwind (m/s)"`
+	SysHeadwind           float64 `json:"sysHeadwind (m/s)"`
+	TailWindPower         float64 `json:"tailWindPower (%)"`
+	HeadWindPower         float64 `json:"headWindPower (%)"`
+	ExpDownhill           float64
+	ExpUphill             float64
+	ExpHeadwind           float64
+	ExpTailwind           float64
+	DownhillTailwindPower float64 `json:"downhillTailwindPower (%)"`
+	DownhillHeadwindPower float64 `json:"downhillHeadwindPower (%)"`
+	CDT                   float64
+	CDH                   float64
+	CUT                   float64
+	CUH                   float64
+}
+
+type riderAndBike struct {
+	CdA            float64 `json:"airDragCoef CdA"`
+	Cd             float64
+	FrontalArea    float64
+	DrivetrainLoss float64 `json:"drivetrainLoss (%)"`
+	Crr            float64 `json:"rollingResistanceCoef"`
+	Cbf            float64 `json:"brakingFrictionCoef"`
+	Ccf            float64 `json:"turnFrictionCoef"`
+}
+
+type environment struct {
+	WindCourse    float64 `json:"windCourse (deg)"`
+	WindSpeed     float64 `json:"windSpeed (m/s)"`
+	BaseElevation float64
+	Temperature   float64 `json:"temperature (C)"`
+	AirDensity    float64 `json:"airDensity (rho)"`
+	AirPressure   float64 `json:"airPressure (hPa)"`
+	Gravity       float64
+}
+
+// New --
+func New(args []string, l logger) (*Parameters, error) {
+
+	p := &Parameters{}
+	p.setSystemDefaultValues()
+
+	cfgfile := "config.json"
+	if len(args) > 3 && args[2] == "-cfg" {
+		cfgfile = args[3]
+	}
+	if len(args) > 5 && args[4] == "-cfg" {
+		cfgfile = args[5]
+	}
+	bytes, err := os.ReadFile(cfgfile)
+	if err != nil {
+		return p, l.Errorf("Config file: %v", err)
+	}
+	if err = json.Unmarshal(bytes, &p); err != nil {
+		return p, l.Errorf("File "+cfgfile+" - %v", err)
+	}
+	p.ConfigJSON = cfgfile
+
+	p.RideJSON = args[1]
+	bytes, err = os.ReadFile(p.RideJSON)
+	if err != nil {
+		return p, l.Errorf("Ride parameter file: %v", err)
+	}
+	if err = json.Unmarshal(bytes, &p); err != nil {
+		return p, l.Errorf("File "+p.RideJSON+" - %v", err)
+	}
+
+	gpxfile := ""
+	if len(args) > 3 && args[2] == "-gpx" {
+		gpxfile = args[3]
+	}
+	if len(args) > 5 && args[4] == "-gpx" {
+		gpxfile = args[5]
+	}
+	if gpxfile != "" {
+		if p.RouteName == "" {
+			p.RouteName = routeNameFromFile(gpxfile)
+		}
+		p.GPXfile = gpxfile
+	}
+
+	if p.ResultDir == "" {
+		return p, nil
+	}
+	if _, err := os.Stat(p.ResultDir); os.IsNotExist(err) {
+		if err := os.Mkdir(p.ResultDir, os.ModeDir); err != nil {
+			//TODO tämä ei aina toimi
+			return p, l.Errorf("Results dir: %v", err)
+		}
+	}
+	return p, nil
+}
+
+func routeNameFromFile(filename string) string {
+	b := []byte(filename)
+	i := 0
+	for i < len(b) && b[i] != '.' {
+		i++
+	}
+	return string(b[0:i])
+}
+
+func (p *Parameters) setSystemDefaultValues() {
+
+	p.GPXfile = ""
+	p.GPXdir = "./gpx/"
+	p.ResultDir = "./results/"
+	p.RouteCSV = false
+	p.ResultTXT = true
+	p.ResultJSON = false
+	p.ParamOutJSON = false
+	p.Logfile = ""
+	p.LogMode = 0
+	p.LogLevel = 1
+	p.UseCRLF = false
+	p.CSVuseComma = false
+	// p.CSVdesimals = 2
+	p.Display = true
+	p.CheckParams = true
+	p.ReportTech = false
+
+	p.Temperature = 20
+
+	p.Filter.Rounds = 2
+	p.Filter.IpoDist = 25
+	p.Filter.IpoSumDist = 150
+	p.Filter.InitialRelGrade = 7
+	p.Filter.MinRelGrade = 0.1
+	p.Filter.BackstepRounds = 0
+	p.Filter.BackstepDist = 0
+	p.Filter.Backsteps = 5
+	p.Filter.LevelFactor = 0.5
+	p.Filter.LevelMax = 4
+	p.Filter.LevelMin = 1
+	p.Filter.MaxFilteredEle = 10
+
+	Q := &p.Powermodel
+	Q.PowermodelType = 1
+	Q.TailWindPower = 85
+	Q.HeadWindPower = 125
+	Q.SysTailwind = -5
+	Q.SysHeadwind = 5
+	Q.ExpDownhill = 2
+	Q.ExpUphill = 0.75
+	Q.ExpHeadwind = 1
+	Q.ExpTailwind = 1
+	Q.DownhillTailwindPower = 5
+	Q.DownhillHeadwindPower = 70
+	Q.CUT = 1
+	Q.CUH = 1
+	Q.CDT = 1
+	Q.CDH = 1
+
+	p.HeavyBrakingPower = -500
+	p.MaxFreewheelPower = 10
+	p.MinFreewheelPower = -75
+	p.LimitTurnSpeeds = false
+	p.LimitDownSpeeds = true
+	p.LimitEntrySpeeds = false
+	// p.SameVelTol = 0.025
+	p.VelLimitGrade = -3
+	p.SteepDownhillGrade = -10
+	p.VerticalUpGrade = 7
+	p.SingleStepBraking = true
+	p.MaxSpeed = 90
+	p.MinLimitedSpeed = 10
+	p.MinSpeed = 3
+
+	p.PowerAcceCoef = 1.1
+	p.PowerAcceMinPros = 50
+	p.PowerDeceCoef = 0.9
+	p.SpeedDeceCoef = 0.5
+
+	p.DeltaVel = 0.5
+	p.DeltaTime = 1.25
+	p.DiffCalc = 1
+	p.VelSolver = 1
+
+	p.NRtol = 0.05
+	p.Bracket = 1.5
+	p.CalcVelError = false
+	p.UseVelTable = false
+	p.SingleStepBraking = true
+}
+
+type attributes struct {
+	min      float64
+	max      float64
+	unit     string
+	notGiven float64
+}
+
+type attributesMap map[string]attributes
+
+func (m attributesMap) put(key string, min, max float64, unit string, notGiven float64) {
+	m[key] = attributes{min, max, unit, notGiven}
+}
+func (m attributesMap) check(val float64, key string, l logger) {
+	a, found := m[key]
+	if !found {
+		l.Err("No attributes found for " + key)
+		return
+	}
+	if a.min <= val && val <= a.max {
+		return
+	}
+	if val == a.notGiven {
+		return
+	}
+	l.Err("Out of range "+key+" =", val, "(range", a.min, "-", a.max, a.unit+" and not given =", a.notGiven, ")")
+}
+
+func getAttributes() attributesMap {
+	m := make(attributesMap, 100)
+	m.put("weight.total", 40, 300, "kg", 0)
+	m.put("windCourse", 0, 360, "degrees", -1)
+	m.put("windSpeed", -10, 10, "m/s", 0)
+	m.put("airDensity", 0.72, 1.5, "", -1)
+	m.put("gravity", 9.780, 9.833, "", -1)
+	m.put("temperature", -25, 45, "deg C", 0)
+	m.put("airPressure", 950, 1085, "hPascals", -1)
+	m.put("baseElevation", 0, 5000, "m", 0)
+	m.put("minSegmentDist", 0, 200, "m", 0)
+
+	m.put("flatSpeed", 10, 60, "km/h", -1)
+	m.put("flatPower", 25, 750, "w", -1)
+
+	m.put("verticalUpSpeed", 150, 2500, "m/h", -1)
+	m.put("uphillPower", 75, 750, "w", -1)
+	m.put("uphillPowerSpeed", 3, 25, "km/h", -1)
+	m.put("uphillPowerGrade", 2, 10, "%", -1)
+
+	m.put("downhillPower", 10, 25, "%", -1)
+	m.put("downhillPowerGrade", -8, -1.5, "%", 0)
+	m.put("maxPedalledSpeed", 15, 70, "km/h", 15)
+	m.put("verticalDownSpeed", 500, 6000, "m/h", -1)
+	m.put("brakingDist", 5, 100, "m", -1)
+
+	m.put("tailWindPower", 50, 100, "%", 0)
+	m.put("sysTailWind", -8, -2, "m/s", 0)
+	m.put("sysHeadwind", 2, 8, "m/s", 0)
+	m.put("headWindPower", 100, 150, "%", 0)
+	m.put("downhillHeadwindPower", 50, 125, "%", 0)
+	m.put("downhillTailwindPower", 1, 20, "%", 0)
+
+	m.put("decelerationPowerFactor", 0.5, 1, "", 0)
+	m.put("accelerationPowerFactor", 1, 2, "", 0)
+	m.put("minAccelerationPower", 0, 200, "%", 0)
+	m.put("decelerationSpeedFactor", 0, 1, "", 0)
+
+	m.put("rollingResistanceCoef", 0.001, 0.02, "", 0)
+	m.put("brakingFrictionCoef", 0.1, 0.5, "", 0)
+	m.put("turnFrictionCoef", 0.05, 0.5, "", 0)
+	m.put("airDragCoef CdA", 0.1, 1.5, "", -1)
+	m.put("drivetrainLoss", 1, 20, "%", 0)
+
+	m.put("maxSpeed", 20, 80, "km/h", 0)
+	m.put("minSpeed", 2, 15, "km/h", 0)
+	m.put("minLimitedSpeed", 5, 15, "km/h", 0)
+	m.put("maxFreewheelPower", 0, 50, "w", 0)
+	m.put("minFreewheelPower", -100, 0, "w", 0)
+	m.put("heavyBrakingPower", -750, -100, "w", 0)
+
+	m.put("uphillBreak.powerLimit", 75, 100, "%", 0)
+	m.put("uphillBreak.breakDuration", 1, 20, "min", 0)
+	m.put("uphillBreak.climbDuration", 5, 60, "min", 0)
+	m.put("filter.levelFactor", 0.1, 1, "", 0)
+	m.put("filter.levelMax", 1, 99, "m", 0)
+	return m
+}
+
+// Check ---
+func (p *Parameters) Check(l logger) error {
+	l.SetPrefix("param:      ")
+	Q := &p.Powermodel
+
+	if Q.FlatSpeed <= 0 && Q.FlatPower <= 0 {
+		l.Err("flatSpeed <= 0 and flatPower <= 0")
+	}
+	if Q.VerticalUpSpeed <= 0 && Q.UphillPower <= 0 {
+		l.Err("verticalUpSpeed <= 0 and uphillPower <= 0")
+	}
+	if Q.UphillPowerSpeed <= 0 && Q.UphillPowerGrade <= 0 {
+		l.Err("uphillPowerSpeed <= 0 and uphillPowerGrade <= 0")
+	}
+	if Q.MaxPedalledSpeed <= 0 && Q.DownhillPowerGrade <= 0 {
+		l.Err("maxPedalledSpeed <= 0 and downhillPowerGrade <= 0")
+	}
+	if p.CdA <= 0 && (Q.FlatSpeed <= 0 || Q.FlatPower <= 0) {
+		l.Err("airDragCoef CdA <= 0 and (flatSpeed <= 0 or flatPower <= 0)")
+	}
+	U := p.UphillBreak
+	if U.ClimbDuration > 0 && U.BreakDuration > 0 && U.BreakDuration > U.ClimbDuration {
+		l.Err("uphillBreakDuration  > uphillBreakTimeBracket")
+	}
+	if p.Weight.Total <= 0 {
+		p.Weight.Total = p.Weight.Bike + p.Weight.Rider + p.Weight.Luggage
+	}
+	if p.CheckParams {
+		m := getAttributes()
+		checkParamRange(p, m, l)
+	}
+	if l.Errors() > 0 {
+		return l.Errorf(" ")
+	}
+	return nil
+}
+
+func checkParamRange(p *Parameters, m attributesMap, l logger) {
+
+	m.check(p.Weight.Total, "weight.total", l)
+	m.check(p.WindCourse, "windCourse", l)
+	m.check(p.WindSpeed, "windSpeed", l)
+	m.check(p.AirDensity, "airDensity", l)
+	m.check(p.Gravity, "gravity", l)
+	m.check(p.Temperature, "temperature", l)
+	m.check(p.AirPressure, "airPressure", l)
+	m.check(p.BaseElevation, "baseElevation", l)
+	m.check(p.VerticalDownSpeed, "verticalDownSpeed", l)
+	m.check(p.BrakingDist, "brakingDist", l)
+
+	Q := &p.Powermodel
+	m.check(Q.FlatSpeed, "flatSpeed", l)
+	m.check(Q.FlatPower, "flatPower", l)
+	m.check(Q.VerticalUpSpeed, "verticalUpSpeed", l)
+	m.check(Q.UphillPower, "uphillPower", l)
+	m.check(Q.UphillPowerSpeed, "uphillPowerSpeed", l)
+	m.check(Q.UphillPowerGrade, "uphillPowerGrade", l)
+	m.check(Q.DownhillPowerGrade, "downhillPowerGrade", l)
+	m.check(Q.MaxPedalledSpeed, "maxPedalledSpeed", l)
+	m.check(Q.TailWindPower, "tailWindPower", l)
+	m.check(Q.SysTailwind, "sysTailWind", l)
+	m.check(Q.SysHeadwind, "sysHeadwind", l)
+	m.check(Q.HeadWindPower, "headWindPower", l)
+	m.check(Q.DownhillPower, "downhillPower", l)
+	m.check(Q.DownhillHeadwindPower, "downhillHeadwindPower", l)
+	m.check(Q.DownhillTailwindPower, "downhillTailwindPower", l)
+
+	m.check(p.PowerDeceCoef, "decelerationPowerFactor", l)
+	m.check(p.PowerAcceCoef, "accelerationPowerFactor", l)
+	m.check(p.PowerAcceMinPros, "minAccelerationPower", l)
+	m.check(p.SpeedDeceCoef, "decelerationSpeedFactor", l)
+	m.check(p.MinSpeed, "minSpeed", l)
+	m.check(p.MinLimitedSpeed, "minLimitedSpeed", l)
+
+	m.check(p.Crr, "rollingResistanceCoef", l)
+	m.check(p.Cbf, "brakingFrictionCoef", l)
+	m.check(p.Ccf, "turnFrictionCoef", l)
+	m.check(p.CdA, "airDragCoef CdA", l)
+	m.check(p.DrivetrainLoss, "drivetrainLoss", l)
+	m.check(p.MaxFreewheelPower, "maxFreewheelPower", l)
+	m.check(p.MinFreewheelPower, "minFreewheelPower", l)
+	m.check(p.HeavyBrakingPower, "heavyBrakingPower", l)
+	m.check(p.UphillBreak.PowerLimit, "uphillBreak.powerLimit", l)
+	m.check(p.UphillBreak.BreakDuration, "uphillBreak.breakDuration", l)
+	m.check(p.UphillBreak.ClimbDuration, "uphillBreak.climbDuration", l)
+	m.check(p.Filter.LevelFactor, "filter.levelFactor", l)
+	m.check(p.Filter.LevelMax, "filter.levelMax", l)
+	m.check(p.Filter.MinSegDist, "minSegmentDist", l)
+}
+
+// UnitConversionIn --
+func (p *Parameters) UnitConversionIn() {
+
+	p.PowerIn = (100 - p.DrivetrainLoss) / 100
+	p.PowerOut = 1 / p.PowerIn
+
+	q := &p.Powermodel
+	q.FlatSpeed *= kmh2ms
+	q.MaxPedalledSpeed *= kmh2ms
+	q.UphillPowerSpeed *= kmh2ms
+	q.UphillPowerGrade /= 100
+	q.DownhillPowerGrade /= 100
+	//Voimansiirtohäviö poistetaan heti ja palautetaan vasta tuloksiin
+	q.FlatPower *= p.PowerIn
+	q.UphillPower *= p.PowerIn
+
+	p.MinSpeed *= kmh2ms
+	p.MaxSpeed *= kmh2ms
+	p.MinLimitedSpeed *= kmh2ms
+	p.UphillBreak.PowerLimit /= 100
+	p.UphillBreak.ClimbDuration *= min2sec
+	p.UphillBreak.BreakDuration *= min2sec
+	p.PowerAcceMinPros /= 100
+	p.VelLimitGrade /= 100
+	p.VerticalUpGrade /= 100
+	p.Filter.InitialRelGrade /= 100
+	p.Filter.MinRelGrade /= 100
+	p.SteepDownhillGrade /= 100
+	p.MaxFreewheelPower *= p.PowerIn
+
+}
+
+// UnitConversionOut --
+func (p *Parameters) UnitConversionOut() {
+	q := &p.Powermodel
+	q.FlatSpeed *= ms2kmh
+	q.UphillPowerSpeed *= ms2kmh
+	q.MaxPedalledSpeed *= ms2kmh
+	q.UphillPowerGrade *= 100
+	q.DownhillPowerGrade *= 100
+	q.FlatPower *= p.PowerOut
+	q.UphillPower *= p.PowerOut
+
+	p.MaxSpeed *= ms2kmh
+	p.MinLimitedSpeed *= ms2kmh
+	p.MinSpeed *= ms2kmh
+	p.UphillBreak.PowerLimit *= 100
+	p.UphillBreak.ClimbDuration *= sec2min
+	p.UphillBreak.BreakDuration *= sec2min
+
+	p.PowerAcceMinPros *= 100
+	p.VelLimitGrade *= 100
+	p.VerticalUpGrade *= 100
+	p.Filter.InitialRelGrade *= 100
+	p.Filter.MinRelGrade *= 100
+	p.SteepDownhillGrade *= 100
+	p.MinPedalledGrade *= 100
+	p.MaxFreewheelPower *= p.PowerOut
+}
+
+// WriteJSON ---
+func (p *Parameters) WriteJSON(l logger) error {
+	if !p.ParamOutJSON {
+		return nil
+	}
+	b, e := json.MarshalIndent(p, "", "\t")
+	if e != nil {
+		return l.Errorf("Param json file: %v", e)
+	}
+	jsonfile := p.ResultDir + p.RouteName + "_parameters" + ".json"
+	if e := os.WriteFile(jsonfile, b, 0644); e != nil {
+		return l.Errorf("Param json file: %v", e)
+	}
+	return nil
+}
