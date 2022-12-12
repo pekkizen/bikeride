2143ccea1b7ccd86334ef1009951eb7059a655ca
pekkizen <60615893+pekkizen@users.noreply.github.com>
2022-12-12 16:41:26 +0200

    Add files via upload

--- route/filter.go
+++ route/filter.go
@@ -4,9 +4,10 @@ import "math"
 
 func (o *Route) Filter() {
 
-	prev := 0
 	f := &o.filter
 	gLim := f.initRelgrade
+	minRelGrade := f.minRelGrade
+	prev := 0
 	gDecrease := 0.5
 
 	if f.rounds < 1 {
@@ -15,27 +16,32 @@ func (o *Route) Filter() {
 		}
 		return
 	}
-	if f.rounds == 1 {
-		gLim = min(0.035, gLim)
+	if minRelGrade <= 0 {
+		minRelGrade = 0.001
+	}
+	if gLim <= 0 {
+		gLim = 0.04
+		if f.rounds > 1 {
+			gLim = 0.08
+		}
 	}
 	if f.rounds > 2 {
 		gDecrease = 0.6
 	}
-	if f.ipoSumDist < 4*o.distMedian {
-		f.ipoSumDist = max(150, 4*o.distMedian)
+	if f.ipoSumDist <= 0 {
+		f.ipoSumDist = max(100, 4*o.distMedian)
 	}
-	if f.ipoDist < 1.1*o.distMedian {
+	if f.ipoDist <= 0 {
 		f.ipoDist = max(25, 1.1*o.distMedian)
 	}
 	o.prepareIpo()
 
 	for i := 1; i <= f.rounds; i++ {
-		
-		o.filterBackSteps(gLim)
-		// o.filterBackStepDist(gLim)
 
-		if gLim *= gDecrease; gLim < f.minRelGrade {
-			gLim = f.minRelGrade
+		o.filterBackSteps(gLim)
+
+		if gLim *= gDecrease; gLim < minRelGrade {
+			gLim = minRelGrade
 		}
 		if prev == f.ipolations {
 			f.rounds = i
@@ -45,9 +51,6 @@ func (o *Route) Filter() {
 	}
 	if o.filter.levelFactor > 0 {
 		o.filterLevel()
-	}
-	if false {
-		o.filterBackStepDist(gLim)
 	}
 	o.recalcDist()
 }
@@ -101,14 +104,14 @@ func (o *Route) level(j int) {
 
 	I, J := &o.route[j-1], &o.route[j]
 	if I.grade*J.grade > -0.005*0.005 {
-		return 
+		return
 	}
 	K := &o.route[j+1]
 	max, min := I.ele, K.ele
 	if max < min {
 		max, min = min, max
 	}
-	
+
 	f := &o.filter
 	f.levelations++
 
@@ -136,7 +139,7 @@ func (o *Route) level(j int) {
 	J.ele += dEle
 	I.grade = (J.ele - I.ele) / I.distHor
 	J.grade = (K.ele - J.ele) / J.distHor
-	f.eleLeveled +=  math.Abs(dEle)
+	f.eleLeveled += math.Abs(dEle)
 }
 
 func (o *Route) filterBackSteps(gradeLim float64) {
@@ -144,86 +147,33 @@ func (o *Route) filterBackSteps(gradeLim float64) {
 		ipo, backsteps = 0, 0
 		enough         = 5 * o.segments
 		r              = o.route
-		backstepLim    = o.filter.backsteps
+		stepLim        = o.filter.backsteps
 		segments       = o.segments
 		maxFiltered    = o.filter.maxFilteredEle
 	)
 	r[1].interpolable = false
 
 	for j := 2; j <= segments; j++ {
-
-		if !r[j].interpolable {
+		s := &r[j]
+		if !s.interpolable {
 			backsteps = 0
 			continue
 		}
-		if math.Abs(r[j-1].grade-r[j].grade) < gradeLim {
+		if math.Abs(r[j-1].grade-s.grade) < gradeLim { 
 			backsteps = 0
 			continue
 		}
-		if math.Abs(r[j].ele-r[j].eleGPX) > maxFiltered {
+		if math.Abs(s.ele-s.eleGPX) > maxFiltered {
 			backsteps = 0
 			continue
 		}
 		r.interpolate(j)
 		ipo++
-		if backsteps < backstepLim {
+		if backsteps < stepLim {
 			j -= 2
 			backsteps++
 		}
 		if ipo > enough {
-			break
-		}
-	}
-	o.filter.ipolations += ipo
-}
-
-func (o *Route) filterBackStepDist(gradeLim float64) {
-	const big = int(1e+9)
-	var (
-		maxi, ipo, right = 0, 0, 0
-		dist             = 0.0
-		left             = big
-		enough           = 10 * o.segments
-		r                = o.route
-		backstepDist     = o.filter.backstepDist
-		segments         = o.segments
-		maxFiltered    = o.filter.maxFilteredEle
-	)
-	r[1].interpolable = false
-
-	for i := 1; i < segments; i++ {
-		j := i + 1
-		if maxi < i {
-			maxi = i
-		}
-		if !r[j].interpolable || math.Abs(r[j].ele-r[j].eleGPX) > maxFiltered {
-			continue
-		}
-		if math.Abs(r[i].grade-r[j].grade) < gradeLim {
-			if i == maxi {
-				//new segment and no interpolation -> reset counters
-				left, right, dist = big, 0, 0
-			}
-			continue
-		}
-		r.interpolate(j)
-		ipo++
-		if j < left || j > right {
-			if j > right {
-				right = j
-			} else {
-				left = j
-			}
-			dist += r[j].distHor
-			if dist > backstepDist {
-				i = maxi
-				continue //to next new segment
-			}
-		}
-		i -= 2 //backstep to previous segment
-
-		if ipo > enough {
-			//here with long backstepDist and small initial gradeLim
 			break
 		}
 	}
